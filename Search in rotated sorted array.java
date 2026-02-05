05e810079e6c2b894918b0b58b6b201d99611c27
Divya singh <diyasingh2056@gmail.com>
2026-02-05 16:34:52 +0530

    Create Search in rotated sorted array.java

---
+++ Search in rotated sorted array.java
@@ -0,0 +1,28 @@
+class Solution {
+    int search(int[] arr, int key) {
+        // code here
+        int n = arr.length;
+        int low = 0,high=n-1;
+        
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(arr[mid] == key)return mid;
+            
+            if(arr[low]<=arr[mid]){
+                if(key>=arr[low] && key<arr[mid]){
+                    high = mid-1;
+                }else{
+                    low = mid+1;
+                }
+            }
+            else{
+                if(key>arr[mid] &&  key<=arr[high]){
+                    low = mid+1;
+                }else{
+                    high = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+}
